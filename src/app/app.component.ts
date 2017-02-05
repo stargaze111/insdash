29f28fa50e6fee52356f4ba904030c4f26cadc8a
chaitanya mokkapati <chaitanya mokkapati>
2017-02-05 16:36:19 +1100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My first Angular 2 App</h1>'
+    templateUrl: './app/resources/dashboard.html'
 })
 export class AppComponent { }

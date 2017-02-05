97ffda9061b4da754760daac24f788fccd8e351e
chaitanya mokkapati <chaitanya mokkapati>
2017-02-05 19:06:50 +1100

    test

--- src/app/insurance/components/index.ts
+++ src/app/insurance/components/index.ts
@@ -52,7 +52,8 @@ import * as moment from 'moment';
 
 @Component({
     selector: 'insurance-dashboard-widget',
-    templateUrl: './src/app/widgets/dashboard.html'
+    templateUrl: './src/app/insurance/components/dashboard.component.html',
+    styleUrls: ['./src/app/insurance/components/dashboard.component.css'],
 })
 
 

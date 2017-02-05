5904bda8a966a5ffd80a0208bd82349639552d75
chaitanya mokkapati <chaitanya mokkapati>
2017-02-05 19:15:28 +1100

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
 import { HttpModule, JsonpModule } from '@angular/http';
 import {Ng2GoogleChartModule} from 'ng2-googlechart';
-import { APP_BASE_HREF } from '@angular/common';
 import { InsuranceModule } from './insurance/insurance.module';
 import { AppComponent }         from './app.component';
 import { EmitterService }          from './emitter.service';
